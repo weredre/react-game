a85e4e2af145d5c7a945415b5a821b40e1724092
Andre Heslop <andreheslop@gmail.com>
2020-10-23 15:43:56 -0400

    login handler is broken

--- gaming-app/src/components/Login.jsx
+++ gaming-app/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form'
-import {login} from '../services/index';
+// import {login} from '../services/index';
 
 class Login extends React.Component {
 
